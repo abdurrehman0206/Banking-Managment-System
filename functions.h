89b41a1924527f1f30d90789fecb1b9d13b6358d
abdurrehman0206 <68471828+abdurrehman0206@users.noreply.github.com>
2022-02-04 20:52:53 +0500

    Added Input and Output (FILES)

--- functions.h
+++ functions.h
@@ -5,7 +5,6 @@
 #include <conio.h>
 #include <windows.h>
 
-
 using namespace std;
 
 struct Person
@@ -13,19 +12,29 @@ struct Person
     string fname, sname;
     int accountId, balance, loan;
 };
-int accNumChk=0;
-short n;
+
+int accNumChk = 1;
+short n ;
 Person *person = new Person[n];
 string filename = "accounts.dat";
 ifstream fin;
 ofstream fout;
-
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void color(int n);
+void id_assign(int &accNumChk);
+void read_file(Person **person, short *n);
+void i_data(Person **person, short *n);
+void o_data(Person *person, short *n);
+void siz(short *n);
+
 void color(int n)
 {
     SetConsoleTextAttribute(console, n);
 }
-void id_assign(int &accNumChk){
+
+void id_assign(int &accNumChk)
+{
     string garbage;
     int chk = 0;
 
@@ -43,11 +52,41 @@ void id_assign(int &accNumChk){
     accNumChk = chk / 4;
 }
 
+void read_file(Person **person, short *n){
+    siz(n);
+    Person *t = new Person[*n];
+    fin.open(filename, ios::in);
+    if (fin.is_open())
+    {
+        color(10);
+        cout << filename << " access granted!" << endl;
+        cout << endl;
+        color(15);
+        for (int i = 0; i < *n; i++)
+        {
 
+            fin >> t[i].fname;
+            fin >> t[i].sname;
+            fin >> t[i].accountId;
+            fin >> t[i].balance;
+            fin >> t[i].loan;
+        }
+    }
+    else
+    {
+        color(12);
+        cout << "File access denied!" << endl;
+        cout << endl;
+        color(15);
+    }
+    fin.close();
+    *person = t;
+    delete t;
+    cin.ignore();
+}
 
-void i_data(Person **person,short &n)
+void i_data(Person **person, short *n)
 {
-
     short inp;
     cout << "Input ::" << endl;
     cout << "Enter Number of Persons to add :: ";
@@ -56,11 +95,11 @@ void i_data(Person **person,short &n)
     {
         return;
     }
-    n = inp;
+    *n = inp;
     Person *t = new Person[inp];
     cout << endl;
     cin.ignore();
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < *n; i++)
     {
         cout << "Enter Persons First Name :: ";
         getline(cin, t[i].fname);
@@ -72,17 +111,18 @@ void i_data(Person **person,short &n)
         cout << endl;
     }
     *person = t;
-    
-    fout.open(filename , ios::binary | ios::ate | ios::out | ios::app);
+
+    fout.open(filename, ios::binary | ios::ate | ios::out | ios::app);
     if (fout.is_open())
     {
         color(10);
         cout << filename << " access granted!" << endl;
         cout << endl;
         color(15);
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < *n; i++)
         {
-            fout << t[i].fname << "\t" << t[i].sname << "\t" << t[i].accountId << "\t" << t[i].balance << "\t" << t[i].loan << endl;
+            fout << t[i].fname << " " << t[i].sname << " " << t[i].accountId << " " << t[i].balance << " " << t[i].loan << endl;
+            //fout.write((char*)&t , sizeof(t));
         }
     }
     else
@@ -95,4 +135,32 @@ void i_data(Person **person,short &n)
     delete t;
     fout.close();
     cin.ignore();
+}
+
+void o_data(Person *person,short *n){
+    read_file(&person, n);
+    cout << "FirstName \t LastName \t AccID \t\t Balance \t Loan" << endl;
+    for (int i = 0; i < *n; i++)
+    {
+        cout << setw(17) << left << person[i].fname << setw(16) << left << person[i].sname << setw(16) << left << person[i].accountId << setw(16) << left << person[i].balance << person[i].loan << endl;
+    }
+}
+
+
+void siz(short *n)
+{
+    int chk = 0;
+    string garbage;
+    fin.open(filename);
+
+    if (fin.is_open())
+    {
+        while (!fin.eof())
+        {
+            fin >> garbage;
+            chk++;
+        }
+    }
+    fin.close();
+    *n = chk / 5;
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 
 int main(){
     id_assign(accNumChk);
-    i_data(&person, n);
+    //i_data(&person, &n);
+    o_data(person, &n);
     system("pause");
     return 0;
 }
