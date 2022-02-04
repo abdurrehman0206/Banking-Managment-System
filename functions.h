5055196d2efed4d181f883cf3d6abd580c06e65c
abdurrehman0206 <68471828+abdurrehman0206@users.noreply.github.com>
2022-02-04 17:54:57 +0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,12 +1,46 @@
 #include <iostream>
+#include <fstream>
 #include <string>
-
 
 using namespace std;
 
-
-struct Person{
+struct Person
+{
     string fname, sname;
-    int accountId , balance, loan;
-  
+    int accountId, balance, loan;
 };
+short n;
+Person *person = new Person[n];
+string filename;
+
+
+void i_data(Person **person,short &n)
+{
+
+    short inp;
+    cout << "Input ::" << endl;
+    cout << "Enter Number of Courses :: ";
+    cin >> inp;
+    if (inp == 0)
+    {
+        return;
+    }
+    n = inp;
+    filename = "Nil";
+    Person *t = new Person[inp];
+    cout << endl;
+    cin.ignore();
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter Persons First Name :: ";
+        getline(cin, t[i].fname);
+        cout << "Enter " << t[i].fname << "'s Last Name :: ";
+        getline(cin, t[i].sname);
+        t[i].balance = 0;
+        t[i].loan = 0;
+        cout << endl;
+    }
+    *person = t;
+    delete t;
+
+}
